e2a417b0040aaed25f3752fedd5f4f3e7cd5116f
Kevin Wander <87961130+Kwander@users.noreply.github.com>
2024-12-20 22:44:09 -0500

    Resetting back for Debugging

--- MyGame/MyGame.cpp
+++ MyGame/MyGame.cpp
@@ -1,15 +1,16 @@
 #include "Quad.h"
 #include <iostream>
 
-class MyGameApplication : public Quad::QuadApplication {
-	virtual void Initialize() override {
+class MyGameApplication : public Quad::QuadApplication
+{
+	virtual void Initialize() override
+	{
 		QUAD_LOG("Starting...");
 	}
 
-	virtual void Update() override {
-		//std::cout << "Running " << std::endl;
-		
-
+	virtual void Update() override
+	{
+		// std::cout << "Running " << std::endl;
 	}
 };
 
--- Quad/EntryPoint.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define QUAD_GAME_START(ClassName) \
-int main(){\
-	ClassName game;\
-	game.Run();\
-	return 0;\
-}
