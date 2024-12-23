b67df472cb7efed7c33521d58c1aa424713fad5e
Kevin Wander <87961130+Kwander@users.noreply.github.com>
2024-12-23 02:59:25 -0500

    Fix Vertices tilting 3d image issue
    It was tilting on the z axis because vertice was hardcoded 100f

--- Quad/codeOpenGL/OpenGLRenderer.cpp
+++ Quad/codeOpenGL/OpenGLRenderer.cpp
@@ -6,94 +6,86 @@
 #define GLFW_INCLUDE_NONE
 #include "GLFW/glfw3.h"
 
-namespace Quad {
+namespace Quad
+{
 	OpenGLRenderer::OpenGLRenderer()
 	{
 		// start glad
-		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+		{
 			QUAD_ERROR("Failed to initialize GLAD");
 		}
 
 		mDefaultShaders.LoadShaders("../Quad/QuadAssets/Shaders/defaultVertexShader.glsl", "../Quad/QuadAssets/Shaders/defaultFragmentShader.glsl");
 
-
-		//create opengl objects
+		// create opengl objects
 		glGenVertexArrays(1, &mVAO);
 		glBindVertexArray(mVAO);
 
 		glGenBuffers(1, &mVBO);
 		glBindBuffer(GL_ARRAY_BUFFER, mVBO);
 
-		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)0);
 		glEnableVertexAttribArray(0);
 
-		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float))); // possibly WRONG it's out of the screen hes recording in tooo zoomed in.
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(2 * sizeof(float))); // possibly WRONG it's out of the screen hes recording in tooo zoomed in.
 		glEnableVertexAttribArray(1);
 
 		glGenBuffers(1, &mEBO);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mEBO);
 
-		//enable transparency
+		// enable transparency
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	}
-	void OpenGLRenderer::Draw(Image& pic, int xCoord, int yCoord)
+	void OpenGLRenderer::Draw(Image &pic, int xCoord, int yCoord)
 	{
 		float vertices[] = {
-			xCoord, yCoord , 0.0f, 0.0f, 
-			xCoord, yCoord + pic.GetHeight() , 0.0f, 1.0f, 
-			xCoord + pic.GetWidth(), yCoord + pic.GetHeight() , 1.0f, 1.0f, 
-			xCoord + pic.GetWidth(), 100.0f , 1.0f, 0.0f
-		};
+			xCoord, yCoord, 0.0f, 0.0f,
+			xCoord, yCoord + pic.GetHeight(), 0.0f, 1.0f,
+			xCoord + pic.GetWidth(), yCoord + pic.GetHeight(), 1.0f, 1.0f,
+			xCoord + pic.GetWidth(), yCoord, 1.0f, 0.0f};
 
 		unsigned int indices[] = {
-			0, 1, 2, //first triangle
-			0, 2, 3 //second triangle
+			0, 1, 2, // first triangle
+			0, 2, 3	 // second triangle
 		};
 
-		
 		glBindBuffer(GL_ARRAY_BUFFER, mVBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mEBO);
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-
 		mDefaultShaders.Bind();
-		mDefaultShaders.SetIntUniform("ScreenDim", {QuadWindow::GetWindow()->GetWidth(), QuadWindow::GetWindow()->GetHeight() });
+		mDefaultShaders.SetIntUniform("ScreenDim", {QuadWindow::GetWindow()->GetWidth(), QuadWindow::GetWindow()->GetHeight()});
 		glBindVertexArray(mVAO);
 		pic.Bind();
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-		
 	}
-	void OpenGLRenderer::Draw(Image& pic, int xCoord, int yCoord, Shaders& shader)
+	void OpenGLRenderer::Draw(Image &pic, int xCoord, int yCoord, Shaders &shader)
 	{
 		float vertices[] = {
-	xCoord, yCoord , 0.0f, 0.0f,
-	xCoord, yCoord + pic.GetHeight() , 0.0f, 1.0f,
-	xCoord + pic.GetWidth(), yCoord + pic.GetHeight() , 1.0f, 1.0f,
-	xCoord + pic.GetWidth(), 100.0f , 1.0f, 0.0f
-		};
+			xCoord, yCoord, 0.0f, 0.0f,
+			xCoord, yCoord + pic.GetHeight(), 0.0f, 1.0f,
+			xCoord + pic.GetWidth(), yCoord + pic.GetHeight(), 1.0f, 1.0f,
+			xCoord + pic.GetWidth(), yCoord, 1.0f, 0.0f};
 
 		unsigned int indices[] = {
-			0, 1, 2, //first triangle
-			0, 2, 3 //second triangle
+			0, 1, 2, // first triangle
+			0, 2, 3	 // second triangle
 		};
-
 
 		glBindBuffer(GL_ARRAY_BUFFER, mVBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mEBO);
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
 
 		shader.Bind();
 		glBindVertexArray(mVAO);
 		pic.Bind();
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
 	}
 	void OpenGLRenderer::ClearScreen()
 	{
